0cf3676ca840a60a50bbeaac01345b079d436048
CodeWithTheDoctor <ashishithape.ai@gmail.com>
2023-05-06 22:41:55 +0800

    - Added gitignore

--- QB/qbServer.java
+++ QB/qbServer.java
@@ -9,19 +9,47 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
 class QBserver {
-    public static void main(String[] args) throws IOException {
+    // an object called QApair that stores two string values, a question and an
+    // answer
+    static class QApair {
+        String question;
+        String answer;
 
-        int serverPort = 8001;
+        public QApair(String question, String answer) {
+            this.question = question;
+            this.answer = answer;
+        }
+    }
+
+    private final int NUM_QUESTIONS = 4;
+
+    // Array of QApairs that will be used to store the python questions and answers
+    private QApair[] PythonQuestionBank = new QApair[NUM_QUESTIONS];
+
+    // Array of QApairs that will be used to store the C questions and answers
+    private QApair[] JavaQuestionBank = new QApair[NUM_QUESTIONS];
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Usage: java QBserver <port> <language [python/c]>");
+            System.exit(1);
+        }
+
+        // parsing string to int from argument
+        int serverPort = Integer.parseInt(args[0]);
         HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+
         server.createContext("/api/hello", (exchange -> {
             String respText = "Hello!\n";
-
             exchange.sendResponseHeaders(200, respText.getBytes().length);
             OutputStream output = exchange.getResponseBody();
             output.write(respText.getBytes());
             output.flush();
             exchange.close();
         }));
+
+        // Endpoint for fetching
+
         server.setExecutor(null); // creates a default executor
         server.start();
 
