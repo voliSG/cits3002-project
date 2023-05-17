868a5a6882077f984d3559b7f81fa92024057345
Dylan To <dylan.to@hotmail.com>
2023-05-17 23:41:02 +0800

    Fix C code runner

--- QB/QBCodeRunner.java
+++ QB/QBCodeRunner.java
@@ -1,10 +1,7 @@
-package qb;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Writer;
 import java.util.concurrent.TimeUnit;
@@ -21,7 +18,7 @@ public class QBCodeRunner {
         this.timeout = timeout;
     }
 
-    public String run(String code) throws IOException, InterruptedException {
+    public String run(String code) throws IOException, InterruptedException, BadCodeException {
         if (this.language.equals("python")) {
             return runPython(code);
         } else if (this.language.equals("c")) {
@@ -43,24 +40,27 @@ public class QBCodeRunner {
         return file;
     }
 
-    private String compileC(File file) throws IOException, InterruptedException {
-        String fileDirOut = ("gcc " + file.getAbsolutePath() + " -o " + file.getAbsolutePath() + ".out");
+    private File compileC(File file) throws IOException, InterruptedException {
         Process process = Runtime.getRuntime()
-                .exec(fileDirOut);
+                .exec("cc " + file.getAbsolutePath() + " -o " + file.getAbsolutePath() + ".out");
         process.waitFor(timeout, TimeUnit.SECONDS);
 
-        return fileDirOut;
+        File outputFile = new File(file.getAbsolutePath() + ".out");
+
+        return outputFile;
     }
 
-    private String runPython(String code) throws IOException, InterruptedException {
+    private String runPython(String code) throws IOException, InterruptedException, BadCodeException {
         File file = saveCode(code, ".py");
 
         // ! the binary is python3 on unix, and python on windows
-        Process process = Runtime.getRuntime().exec("python " + file.getAbsolutePath());
+        Process process = Runtime.getRuntime().exec("python3 " + file.getAbsolutePath());
         process.waitFor(timeout, TimeUnit.SECONDS);
 
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
         BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+        int exitCode = process.waitFor();
 
         String output = "";
         String o = "";
@@ -74,20 +74,27 @@ public class QBCodeRunner {
             error += e + "\n";
         }
 
+        if (exitCode == 0) {
+            System.out.println("Python program executed successfully.");
+        } else {
+            System.out.println("Python program execution failed with exit code: " + exitCode);
+            throw new BadCodeException("Code running failed");
+        }
+
         if (error.length() > 0) {
-            return error;
+            throw new BadCodeException("runPython code failed");
         }
 
         return output;
     }
 
-    private String runC(String code) throws IOException, InterruptedException {
+    private String runC(String code) throws IOException, InterruptedException, BadCodeException {
         File file = saveCode(code, ".c");
-        String compiledFileDir = compileC(file);
+        File compiledFile = compileC(file);
 
         Process process = Runtime.getRuntime()
-                .exec(compiledFileDir);
-        // process.waitFor(timeout, TimeUnit.SECONDS);
+                .exec(compiledFile.getAbsolutePath());
+        process.waitFor(timeout, TimeUnit.SECONDS);
 
         // Read the process output
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
@@ -113,6 +120,11 @@ public class QBCodeRunner {
             System.out.println("C program executed successfully.");
         } else {
             System.out.println("C program execution failed with exit code: " + exitCode);
+            throw new BadCodeException("Code running failed");
+        }
+
+        if (error.length() > 0) {
+            throw new BadCodeException("Code running failed");
         }
 
         return output;
@@ -123,8 +135,12 @@ public class QBCodeRunner {
         int timeout = 5;
         QBCodeRunner runner = new QBCodeRunner(language, timeout);
         try {
-            // System.out.println(runner.run("print('Hello World')\nprint('this is a second line')"));
-            System.out.println("> " + runner.run("#include <stdio.h>\n\nint main() {\n\tprintf(\"Hello, World!\\n\");\n\treturn 0;\n}"));
+            // System.out.println(runner.run("print('Hello World')\nprint('this is a second
+            // line')"));
+            System.out.println("> " + runner
+                    .run("#include <stdio.h>\n\nint main() {\n\tprintf(\"Hello, World!\\n\");\n\treturn 0;\n}"));
+        } catch (BadCodeException e) {
+            // don't check the answer if the code is bad
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
